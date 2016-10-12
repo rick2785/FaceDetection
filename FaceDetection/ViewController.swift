083cfbe62f26d3e3b34efd0c50e1b88e2b4b147c
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-10-11 20:32:20 -0500

    first commit

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.imageView.image = UIImage(named: "faceDetection3")
+        self.findFace()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func findFace() {
+        
+        guard let faceImage = CIImage(image: self.imageView.image!) else { return }
+        
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        let faces = faceDetector?.features(in: faceImage, options: [CIDetectorSmile: true, CIDetectorEyeBlink: true])
+        
+        for face in faces as! [CIFaceFeature] {
+            
+            if face.hasSmile {
+                print("😀") // control + command + space brings up symbols
+            }
+            
+            if face.leftEyeClosed {
+                print("Left: 😉")
+            }
+            
+            if face.rightEyeClosed {
+                print("Right: 😉")
+            }
+        }
+        
+        if faces!.count != 0 {
+            print("Number of Faces: \(faces!.count)")
+        } else {
+            print("No Faces 😢")
+        }
+    }
 
 }
 
